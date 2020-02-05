75ef797481b3ab0093199380990857102f490012
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-05 22:35:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   count = 0
   while array[count] do
-    array * array
+    squared_element = array[count] * array[count]
+    puts squared_element
     count += 1
   end
 end
